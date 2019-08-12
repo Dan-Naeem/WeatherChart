ff8e4d37a55956c16dff40b1b0734cddad50549a
Dan Naeem <dan.naeem95@gmail.com>
2019-08-12 15:28:27 -0400

    add states to App class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div>
-      <div class="jumbotron">
-        <h1>
-          Test Title
-        </h1>
-        <br />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <p>
-          Test paragraph
-        </p>
+    this.state = {
+      string : "blank",
+      number : 10,
+    }
+  }
+  render() {
+    return (
+      <div class="p-0 mx-2 d-flex flex-column">
+        <div class="jumbotron">
+          <h1>
+            Test Title
+          </h1>
+          <br />
+
+          <p>
+            Test paragraph
+          </p>
+        </div>
+
+        <div class="jumbotron">
+          <p>the string is {this.state.string}</p>
+          <p>the number is {this.state.number}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
